cebe2bc9d92be6b2647dd725c0f94fd9ae8453ed
bqbbo <bobbye23899@gmail.com>
2026-01-19 19:26:06 -0800

    Create basic application with vite and react, add backend support

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # rbcyber.org Website
 
 Frontend and backend for [rbcyber.org](https://rbcyber.org/)
+
+## Setup
+
+- See /src/config/example.env for necessary setup
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+import dotenv from "dotenv";
+
+import logger from "./middleware/logger.js";
+
+import healthRouter from "./routes/health.js";
+
+dotenv.config({ path: "./config/.env" });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const distPath = path.join(__dirname, "../client/dist");
+
+const app = express();
+
+const PORT = process.env.RBCYBER_PORT || 3000; // Configure with dotenv, default to 3000
+
+app.use(logger);
+app.use("/api/health", healthRouter);
+
+app.use(express.static(distPath));
+
+app.get(/(.*)/, (_, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+});
+
+app.listen(PORT, () =>
+    console.log(`Express is server running on http://localhost:${PORT}`),
+);
