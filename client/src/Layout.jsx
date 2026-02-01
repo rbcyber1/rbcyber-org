b53dd1010e3da5bb5e308ab0974a08ba4d454de9
bqbbo <bobbye23899@gmail.com>
2026-02-01 12:32:29 -0800

    Fix CSS animations

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Header from "./elements/Header";
 import Navbar from "./elements/Navbar";
@@ -7,12 +7,16 @@ import Footer from "./elements/Footer";
 import "./styles/Layout.css";
 
 export default function Layout() {
+    const location = useLocation();
+
     return (
         <div className="layout-container">
             <Header />
             <Navbar />
             <main className="main-content">
-                <Outlet />
+                <div key={location.pathname} className="page-transition fadeIn">
+                    <Outlet key={location.pathname} />
+                </div>
             </main>
             <Footer />
         </div>
