3048c12ce0e21482fa444d8513929ec1705660f1
bqbbo <107454678+bqbbo@users.noreply.github.com>
2026-08-16 21:51:19 -0700

    Sudoer issues

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,5 @@ cd $RBCYBER_WEBSITE_DIR
 git pull
 npm install
 npm run build
-sudo cp -r ./apps/frontend/dist/* /var/www/rbcyber.org/html/
+cp -r ./apps/frontend/dist/* /var/www/rbcyber.org/html/
 sudo systemctl restart rbcyber-web # Assumes a systemd service is set up for the backend server.
