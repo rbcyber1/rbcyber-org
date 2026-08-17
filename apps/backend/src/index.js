a7c2bd697c11f97fe366dab1907243305a9bf88e
bqbbo <107454678+bqbbo@users.noreply.github.com>
2026-08-16 20:28:45 -0700

    Dev environment: Vite middleware

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -6,25 +6,40 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const frontendRoot = path.resolve(__dirname, "../../frontend");
 
 config({ path: path.resolve(__dirname, "../config/.env"), quiet: true });
 
 const app = express();
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("rbcyber.org is under maintenance. Please check back later.");
-});
+if (process.env.NODE_ENV !== "production") {
+    console.log("RUNNING DEV");
+    // Dev: Express acts as the dev server.
+    // Vite transforms client side code and serves it to the backend on the fly.
+    const { createServer: createViteServer } = await import("vite");
+    const vite = await createViteServer({
+        root: frontendRoot,
+        server: { middlewareMode: true },
+        appType: "spa",
+    });
+    app.use(vite.middlewares);
+} else {
+    // Prod: This case is an option for users not running our infrastructure.
+    // Serve to this backend itself instead of a reverse proxy.
+    app.use(express.static(path.join(clientRoot, "dist")));
+    app.use((req, res) => {
+        res.sendFile(path.join(clientRoot, "dist", "index.html"));
+    });
+}
+
+if (!process.env.RBCYBER_WEB_PORT) {
+    console.error("Error: RBCYBER_WEB_PORT environment variable is not set.");
+    process.exit(1);
+}
 
 app.listen(process.env.RBCYBER_WEB_PORT, () => {
-    if (!process.env.RBCYBER_WEB_PORT) {
-        console.error(
-            "Error: RBCYBER_WEB_PORT environment variable is not set.",
-        );
-        process.exit(1);
-    }
-
     console.log(
         `rbcyber-web-backend is running on port ${process.env.RBCYBER_WEB_PORT}`,
     );
---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+    return (
+        <div>
+            <h1>rbcyber.org is currently under maintenance</h1>
+            <p>
+                The website is undergoing a complete overhaul. Check our{" "}
+                <a href="https://www.instagram.com/rbhscyber/">Instagram</a> or{" "}
+                <a href="https://discord.gg/jEPCcVGGYW">Discord</a> for updates.
+            </p>
+        </div>
+    );
+};
+
+export default App;
