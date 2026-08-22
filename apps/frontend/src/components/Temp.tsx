bf81d464818863976e5dd8688afa34950bb89b04
bqbbo <107454678+bqbbo@users.noreply.github.com>
2026-08-21 22:51:35 -0700

    Test commit, add period

--- apps/frontend/src/components/Temp.tsx
+++ apps/frontend/src/components/Temp.tsx
@@ -1,7 +1,7 @@
 const Temp = () => {
     return (
         <div>
-            <h1>rbcyber.org is currently under maintenance</h1>
+            <h1>rbcyber.org is currently under maintenance.</h1>
             <p>
                 The website is undergoing a complete overhaul. Check our{" "}
                 <a href="https://www.instagram.com/rbhscyber/">Instagram</a> or{" "}
